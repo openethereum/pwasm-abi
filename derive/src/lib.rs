e5c7c8b072d095341408f85de868ab093488493c
NikVolf <nikvolf@gmail.com>
2017-09-17 22:29:36 +0300

    general stubs

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,2 +1,12 @@
+extern crate proc_macro;
 extern crate pwasm_abi as abi;
+extern crate syn;
+#[macro_use]
+extern crate quote;
 
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(LegacyDispatch, attributes(dispatch))]
+pub fn derive_legacy_dispatch(input: TokenStream) -> TokenStream {
+	input
+}
