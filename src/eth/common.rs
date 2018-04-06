103e602eb8570e2d2d14c047b2dcb4f31d9e9f01
NikVolf <nikvolf@gmail.com>
2018-04-06 14:21:05 +0300

    fixed length arrays & H256

--- src/eth/common.rs
+++ src/eth/common.rs
@@ -2,7 +2,7 @@
 
 use lib::*;
 use super::{util, Stream, AbiType, Sink, Error};
-use parity_hash::Address;
+use parity_hash::{Address, H256};
 use bigint::U256;
 
 impl AbiType for u32 {
@@ -121,17 +121,25 @@ impl AbiType for U256 {
 
 impl AbiType for Address {
 	fn decode(stream: &mut Stream) -> Result<Self, Error> {
-		stream.advance(32)?;
-
-		Ok(
-			Address::from(&stream.payload()[stream.position()-20..stream.position()])
-		)
+		let arr = <[u8; 20]>::decode(stream)?;
+		Ok(arr.into())
 	}
 
 	fn encode(self, sink: &mut Sink) {
-		let tail = sink.preamble_mut().len();
-		sink.preamble_mut().resize(tail + 32, 0);
-		sink.preamble_mut()[tail+12..tail+32].copy_from_slice(self.as_ref());
+		self.0.encode(sink)
+	}
+
+	const IS_FIXED: bool = true;
+}
+
+impl AbiType for H256 {
+	fn decode(stream: &mut Stream) -> Result<Self, Error> {
+		let arr = <[u8; 32]>::decode(stream)?;
+		Ok(arr.into())
+	}
+
+	fn encode(self, sink: &mut Sink) {
+		self.0.encode(sink)
 	}
 
 	const IS_FIXED: bool = true;
@@ -227,4 +235,113 @@ impl AbiType for i64 {
 	}
 
 	const IS_FIXED: bool = true;
+}
+
+macro_rules! abi_type_fixed_impl {
+	($num: expr) => {
+		impl AbiType for [u8; $num] {
+			fn decode(stream: &mut Stream) -> Result<Self, Error> {
+				let previous_position = stream.advance(32)?;
+				let slice = &stream.payload()[previous_position..stream.position()];
+				let mut result = [0u8; $num];
+				result.copy_from_slice(&slice[32-$num..32]);
+				for padding_byte in slice.iter().take(32-$num) {
+					if *padding_byte != 0 {
+						return Err(Error::InvalidPadding);
+					}
+				}
+				Ok(result)
+			}
+
+			fn encode(self, sink: &mut Sink) {
+				let mut padded = [0u8; 32];
+				padded[32-$num..32].copy_from_slice(&self[..]);
+				sink.preamble_mut().extend_from_slice(&padded[..]);
+			}
+
+			const IS_FIXED: bool = true;
+		}
+	}
+}
+
+abi_type_fixed_impl!(1);
+abi_type_fixed_impl!(2);
+abi_type_fixed_impl!(3);
+abi_type_fixed_impl!(4);
+abi_type_fixed_impl!(5);
+abi_type_fixed_impl!(6);
+abi_type_fixed_impl!(7);
+abi_type_fixed_impl!(8);
+abi_type_fixed_impl!(9);
+abi_type_fixed_impl!(10);
+abi_type_fixed_impl!(11);
+abi_type_fixed_impl!(12);
+abi_type_fixed_impl!(13);
+abi_type_fixed_impl!(14);
+abi_type_fixed_impl!(15);
+abi_type_fixed_impl!(16);
+abi_type_fixed_impl!(17);
+abi_type_fixed_impl!(18);
+abi_type_fixed_impl!(19);
+abi_type_fixed_impl!(20);
+abi_type_fixed_impl!(21);
+abi_type_fixed_impl!(22);
+abi_type_fixed_impl!(23);
+abi_type_fixed_impl!(24);
+abi_type_fixed_impl!(25);
+abi_type_fixed_impl!(26);
+abi_type_fixed_impl!(27);
+abi_type_fixed_impl!(28);
+abi_type_fixed_impl!(29);
+abi_type_fixed_impl!(30);
+abi_type_fixed_impl!(31);
+abi_type_fixed_impl!(32);
+
+#[cfg(test)]
+mod tests {
+
+	use super::super::{Stream, Sink};
+
+	#[test]
+	fn fixed_array_padding() {
+		let data = &[
+			0u8, 1u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8,
+			0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8
+		];
+
+		let mut stream = Stream::new(data);
+
+		let val: [u8; 31] = stream.pop().expect("fixed array failed to deserialize");
+
+		assert_eq!(val,
+			[
+				1u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8,
+				0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8
+			]
+		);
+
+		let mut sink = Sink::new(1);
+		sink.push(val);
+
+		assert_eq!(&sink.finalize_panicking()[..], &data[..]);
+	}
+
+	#[test]
+	fn fixed_array_padding_2() {
+		let data = &[
+			0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8,
+			0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 0u8, 1u8, 2u8
+		];
+
+		let mut stream = Stream::new(data);
+
+		let val: [u8; 2] = stream.pop().expect("fixed array failed to deserialize");
+
+		assert_eq!(val, [1u8, 2u8]);
+
+		let mut sink = Sink::new(1);
+		sink.push(val);
+
+		assert_eq!(&sink.finalize_panicking()[..], &data[..]);
+	}
 }
