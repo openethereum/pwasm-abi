b8b19a4eab93fb540e3093691898f183b08232f2
fro <frol.rage@gmail.com>
2018-04-10 12:02:22 +0200

    rename test mod

--- tests/src/trivia.rs
+++ tests/src/trivia.rs
@@ -9,10 +9,7 @@ use pwasm_abi_derive::eth_abi;
 #[eth_abi(Endpoint, Client)]
 pub trait Contract {
 	fn constructor(&mut self, _p: bool);
-
-	fn baz(&mut self, _p1: u32, _p2: bool);
-	fn boo(&mut self, _arg: u32) -> u32;
-	fn sam(&mut self, _p1: Vec<u8>);
+	fn sam(&mut self, _p1: Vec<u8>) -> u32;
 
 	#[event]
 	fn baz_fired(&mut self, indexed_p1: u32, p2: u32);
@@ -23,12 +20,8 @@ struct TestContractInstance;
 impl Contract for TestContractInstance {
 	fn constructor(&mut self, _p1: bool) {
 	}
-	fn baz(&mut self, _p1: u32, _p2: bool) {
-	}
-	fn boo(&mut self, _arg: u32) -> u32 {
+	fn sam(&mut self, _p1: Vec<u8>) -> u32 {
 		0
-	}
-	fn sam(&mut self, _p1: Vec<u8>) {
 	}
 }
 
