e23ef4d6a431c5213124ab94583808c3cdfa89b7
fro <frol.rage@gmail.com>
2018-04-10 12:12:24 +0200

    impl removed

--- tests/src/trivia.rs
+++ tests/src/trivia.rs
@@ -14,18 +14,3 @@ pub trait Contract {
 	#[event]
 	fn baz_fired(&mut self, indexed_p1: u32, p2: u32);
 }
-
-struct TestContractInstance;
-
-impl Contract for TestContractInstance {
-	fn constructor(&mut self, _p1: bool) {
-	}
-	fn sam(&mut self, _p1: Vec<u8>) -> u32 {
-		0
-	}
-}
-
-#[test]
-fn inner_externs_test() {
-	Endpoint::new(TestContractInstance{});
-}
